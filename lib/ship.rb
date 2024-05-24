f77166daa5e1b44f5b17424e6348756597a9d7af
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-05-23 18:09:44 -0600

    i set up the ship and ship spec

---
+++ lib/ship.rb
@@ -0,0 +1,10 @@
+class Ship
+
+    attr_reader :name, :length, :health
+
+    initialize (name, length, health)
+        @name = name
+        @length = length
+        @health = health
+
+end
---
+++ spec/ship_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+RSpec.describe Ship do
+    before(:each) do
+        cruiser = Ship.new("Cruiser", 3)
+    end
+end
