40ec922df6d1fd3f66ca87b1c13e97077cc66354
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-25 21:40:30 -0600

    Brainstorm runner file / gameplay

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -44,25 +44,23 @@ RSpec.describe Cell do
   describe 'render' do
     it 'populates cell' do
       @cell_1.fire_upon
+
       expect(@cell_1.render).to eq("M")
 
       @cell_2.place_ship(@cruiser)
       
       expect(@cell_2.render).to eq(".")
-      
       expect(@cell_2.render(true)).to eq("S")
       
       @cell_2.fire_upon
       
       expect(@cell_2.render).to eq("H")
-      
       expect(@cruiser.sunk?).to eq(false)
       
       @cruiser.hit
       @cruiser.hit
 
       expect(@cruiser.sunk?).to eq(true)
-      
       expect(@cell_2.render).to eq("X")
     end
   end
